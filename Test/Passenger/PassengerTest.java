5562a86a268b9e1b095e58f216ce5d616cf51fcc
kasimibrahim <kasim.ibrahim@ashesi.edu.gh>
2022-06-22 15:52:36 -0700

    Tested dating

---
+++ Test/Passenger/PassengerTest.java
@@ -0,0 +1,34 @@
+package Passenger;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PassengerTest {
+
+    @Test
+    public void humanCanDateTheirType(){
+        Human male = new Human("Kasim");
+        Human female = new Human("Linda");
+        Assert.assertTrue(male.canDate(female));
+
+    }
+
+    @Test
+    public void martianCanDateTheirType(){
+
+        Martian m = new Martian("Goppy");
+        Martian n = new Martian("Hoppy");
+        Assert.assertTrue(m.canDate(n));
+    }
+
+    @Test
+    public void martianCannotDateHuman(){
+        Martian m = new Martian("Hoppy");
+        Human h = new Human("Kasim");
+
+        Assert.assertFalse(m.canDate(h));
+    }
+
+}
